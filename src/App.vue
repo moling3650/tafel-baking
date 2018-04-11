fa23d354acfd5346472d4e6ca4298ff911a89db7
moling <365024424@qq.com>
2018-04-11 15:35:28 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <BakingReport/>
+  </div>
+</template>
+
+<script>
+import BakingReport from '@/components/BakingReport'
+
+export default {
+  name: 'App',
+  components: {
+    BakingReport
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/BakingReport.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="BakingReport">BakingReport</div>
+</template>
+
+<script>
+export default {
+  name: 'BakingReport',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
