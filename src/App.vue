094e4577a3cd963c4e8b382d22ad492d7f9df9e1
moling <365024424@qq.com>
2018-04-12 08:56:40 +0800

    添加页面初始化样式

--- src/App.vue
+++ src/App.vue
@@ -16,4 +16,17 @@ export default {
 </script>
 
 <style>
+html,
+body,
+#app {
+  position: relative;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
 </style>
